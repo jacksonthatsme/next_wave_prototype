b4947aca13b689e843b6a6f1c3b9bb1b6463a934
Jackson Lynch <jacksonthatsme@gmail.com>
2013-12-04 05:56:53 -0500

    continued archive, and final edits, removed cname

--- js/nextwave.js
+++ js/nextwave.js
@@ -169,14 +169,19 @@ var mapStyles = [
 /* MAP STYLES */
    var map;
     function initialize() {
+        var myLatLang = new google.maps.LatLng(40.68860, -73.97861);
         var mapOptions = {
           zoom: 15,
-          center: new google.maps.LatLng(40.68860, -73.97861),
+          center: myLatLang,
           scrollwheel: false,
           disableDefaultUI: true,
           styles: mapStyles
         };
       map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      var marker = new google.maps.Marker({
+          position: myLatLang,
+          map: map
+      });
     }
 
 google.maps.event.addDomListener(window, 'load', initialize);
